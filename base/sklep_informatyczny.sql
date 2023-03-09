86eb369a7cdee88577a21565ffeb9cc45a7cc4c8
korapaknss <51759857+korapaknss@users.noreply.github.com>
2023-03-09 11:51:47 +0100

    Add files via upload

--- base/sklep_informatyczny.sql
+++ base/sklep_informatyczny.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 09 Mar 2023, 10:42
+-- Czas generowania: 09 Mar 2023, 11:51
 -- Wersja serwera: 10.4.24-MariaDB
 -- Wersja PHP: 8.1.6
 
@@ -29,8 +29,29 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `categories` (
   `id` int(11) NOT NULL,
-  `name` int(11) NOT NULL
+  `name` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Zrzut danych tabeli `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`) VALUES
+(1, 'Monitory'),
+(2, 'Klawiatury'),
+(3, 'Myszki'),
+(4, 'Słuchawki'),
+(5, 'Obudowy'),
+(6, 'Karty Graficzne'),
+(7, 'Procesory'),
+(8, 'Chłodzenia'),
+(9, 'Pamięć RAM'),
+(10, 'Dyski Twarde'),
+(11, 'Karty Dźwiękowe'),
+(12, 'Karty Sieciowe'),
+(13, 'Zasilacze'),
+(14, 'Mikrofony'),
+(15, 'Konserwacja');
 
 -- --------------------------------------------------------
 
@@ -69,6 +90,20 @@ CREATE TABLE `products` (
   `category_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Zrzut danych tabeli `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `price`, `qt_in_stock`, `category_id`) VALUES
+(1, 'Myszka', 159.99, 40, 3),
+(2, 'Klawiatura', 439.99, 200, 2),
+(3, 'Monitor', 649.99, 154, 1),
+(4, 'Mikrofon', 319.99, 53, 14),
+(5, 'Karta Graficzna', 1999.99, 49, 6),
+(6, 'Chłodzenie', 289.99, 93, 8),
+(7, 'Pamięć RAM', 244.99, 109, 9),
+(8, 'Dysk Twardy', 359.99, 211, 10);
+
 -- --------------------------------------------------------
 
 --
@@ -80,6 +115,22 @@ CREATE TABLE `sales` (
   `percent_off` int(11) NOT NULL,
   `new_price` float NOT NULL,
   `product_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `transactions`
+--
+
+CREATE TABLE `transactions` (
+  `id` int(11) NOT NULL,
+  `price` int(11) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `payment_type` varchar(30) NOT NULL,
+  `delivery_method` varchar(30) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `client_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -112,6 +163,14 @@ ALTER TABLE `sales`
   ADD KEY `product_id` (`product_id`);
 
 --
+-- Indeksy dla tabeli `transactions`
+--
+ALTER TABLE `transactions`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `product_id` (`product_id`),
+  ADD KEY `client_id` (`client_id`);
+
+--
 -- AUTO_INCREMENT dla zrzuconych tabel
 --
 
@@ -119,7 +178,7 @@ ALTER TABLE `sales`
 -- AUTO_INCREMENT dla tabeli `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT dla tabeli `clients`
@@ -131,6 +190,12 @@ ALTER TABLE `clients`
 -- AUTO_INCREMENT dla tabeli `products`
 --
 ALTER TABLE `products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT dla tabeli `transactions`
+--
+ALTER TABLE `transactions`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
@@ -148,6 +213,13 @@ ALTER TABLE `products`
 --
 ALTER TABLE `sales`
   ADD CONSTRAINT `sales_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`);
+
+--
+-- Ograniczenia dla tabeli `transactions`
+--
+ALTER TABLE `transactions`
+  ADD CONSTRAINT `transactions_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`),
+  ADD CONSTRAINT `transactions_ibfk_2` FOREIGN KEY (`client_id`) REFERENCES `clients` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
