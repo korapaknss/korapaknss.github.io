f73e5b08f3002b0f8e82ec587cf3262422b9c0d4
LuxAlone <kzbojak93@gmail.com>
2023-03-09 15:28:08 +0100

    moar

--- JS/menu.js
+++ JS/menu.js
@@ -1,7 +1,8 @@
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
   }
-  
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-  }
+}
