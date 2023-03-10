c1703cac0a570f794ded5adbead3e69d7341e5a4
korapaknss <51759857+korapaknss@users.noreply.github.com>
2023-03-10 20:45:36 +0100

    Update register.php

--- PHP/register.php
+++ PHP/register.php
@@ -8,35 +8,37 @@
 </head>
 <body>
     <header><h1>Rejestracja do Sklepu</h1></header>
-    <form action="..\PHP\register.php" method="post">
+    <form action="" method="post">
         <h3>Dane konta:</h3>
         Nazwa użytkownika: <input type="text" name="login" required><br>
-        Imię: <input type="text" name="name" required><br>
+        <!--Imię: <input type="text" name="name" required><br>
         Nazwisko: <input type="text" name="surname" required><br>
-        Data urodzenia: <input type="date" name="birthdate" required><br>
+        Data urodzenia: <input type="date" name="birthdate" required><br>-->
         Email: <input type="email" name="email" required><br>
         Hasło: <input type="password" name="password" required><br>
-        Powtórz hasło: <input type="password" name="ctr_password" required><br>
+        <!--Powtórz hasło: <input type="password" name="ctr_password" required><br>-->
         <input type="submit" name="register" value="Zarejestruj się!">
     </form>
 
     <?php
     $c = mysqli_connect('localhost', 'root', '', 'sklep_komputerowy');
-
-    $login = $_POST['login'];
-    $name = $_POST['name'];
-    $surname = $_POST['surname'];
-    $birthdate = $_POST['birthdate'];
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-    $ctr_password = $_POST['ctr_password'];
-    $country = $_POST['country'];
-    $city = $_POST['city'];
-    $postal_code = $_POST['postal_code'];
-    $street = $_POST['street'];
-    $house_num = $_POST['house_num'];
-    $apart_num = $_POST['apart_num'];
-    $phone_num = $_POST['phone_num'];
+    if(isset($_POST['login']) && isset($_POST['email']) && isset($_POST['password'])){
+        $login = $_POST['login'];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+    }
+    
+    // $name = $_POST['name'];
+    // $surname = $_POST['surname'];
+    // $birthdate = $_POST['birthdate'];
+    // $ctr_password = $_POST['ctr_password'];
+    // $country = $_POST['country'];
+    // $city = $_POST['city'];
+    // $postal_code = $_POST['postal_code'];
+    // $street = $_POST['street'];
+    // $house_num = $_POST['house_num'];
+    // $apart_num = $_POST['apart_num'];
+    // $phone_num = $_POST['phone_num'];
 
     if(mysqli_query($c, 'SELECT email FROM clients WHERE email = $email;' == $email)){
         echo "Podany adres email jest już w użyciu!";
