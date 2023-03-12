55f9bce727ee7d571b7250607442e3ba04a5446e
korapaknss <51759857+korapaknss@users.noreply.github.com>
2023-03-12 20:20:17 +0100

    Update products_temp.php

--- PHP/products_temp.php
+++ PHP/products_temp.php
@@ -1,10 +1,18 @@
 <?php
+session_start();
 $c = mysqli_connect('localhost', 'root', '', 'sklep_komputerowy');
 
-$q = mysqli_query($c, "SELECT name, price, qt_in_stock FROM products;");
-echo "<ul>";
+$q = mysqli_query($c, "SELECT id, name, price, qt_in_stock FROM products;");
+echo "<form action='' method='post'><ul>";
 while($row = mysqli_fetch_assoc($q)){
-    echo "<li>" . $row['name'] . " " . $row['price'] . "zł " . $row['qt_in_stock'] . "</li><input type='submit' name='add' value='Dodaj do koszyka'><li></li>";
+    echo "<li>" . $row['name'] . " " . $row['price'] . "zł " . $row['qt_in_stock'] . "</li><input type='submit' name='" . $row['id'] . "' value='Dodaj do koszyka'><li></li>";
 }
-echo "</ul>";
+echo "</ul></form>";
+
+while($row1 = mysqli_fetch_assoc($q)){
+    $id = $row1['id'];
+    if(isset($_POST['2'])){
+        echo "Wybrałeś coś!";
+    }
+}
 ?>
