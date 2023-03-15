bd57acc9457ee91a168024573dbdaaf8f2e677bd
korapaknss <51759857+korapaknss@users.noreply.github.com>
2023-03-15 16:02:08 +0100

    Update shopping_cart.php

--- PHP/shopping_cart.php
+++ PHP/shopping_cart.php
@@ -1,3 +1,13 @@
 <?php
+session_start();
 echo "<h1>Twój koszyk:</h1>";
+
+if(isset($_SESSION['cart']) && is_array($_SESSION['cart'])){
+    $cart = $_SESSION['cart'];
+    print_r($cart);
+    echo "<ul>";
+    for($i=0 ; $i<count($cart) ; $i++){
+        echo "<li>" . $cart[15];
+    }
+}
 ?>
