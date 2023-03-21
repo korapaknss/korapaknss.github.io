970d6bc7b6f8af27a31b987d5bb2ee3c89093fcd
LuxAlone <kzbojak93@gmail.com>
2023-03-21 22:26:37 +0100

    add_styl

--- PHP/address_data.php
+++ PHP/address_data.php
@@ -5,24 +5,27 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Rejestracja</title>
+    <link rel="stylesheet" type="text/css" href="..\CSS\style3.css">
 </head>
 <body>
     <header><h1>Rejestracja do Sklepu</h1></header>
-    <form action="" method="post">
-        <h3>Dane adresowe:</h3>
-        Imię: <input type="text" name="name" required><br>
-        Nazwisko: <input type="text" name="surname" required><br>
-        Data urodzenia: <input type="date" name="birthdate" required><br>
-        Kraj: <input type="text" name="country" required><br>
-        Miasto: <input type="text" name="city" required><br>
-        Kod pocztowy: <input type="text" name="postal_code" required><br>
-        Ulica: <input type="text" name="street" required><br>
-        Numer domu: <input type="number" name="house_num" required><br>
-        Numer mieszkania: <input type="number" name="apart_num" required><br>
-        Numer telefonu: <input type="number" name="phone_num" required><br>
-        <input type="checkbox" name="regular">Czy chcesz zapisać się do programu stałego klienta i otrzymywać specjalne zniżki?<br>
+    <div class="reg">
+    <form class="reg_form" action="" method="post" style="font-weight: bold;">
+        <h2>Dane adresowe:</h2><br><br>
+        Imię: <input type="text" name="name" required>&nbsp
+        Nazwisko: <input type="text" name="surname" required>&nbsp
+        Data urodzenia: <input type="date" name="birthdate" required><br><br>
+        Kraj: <input type="text" name="country" required>&nbsp
+        Miasto: <input type="text" name="city" required>&nbsp
+        Kod pocztowy: <input type="text" name="postal_code" required><br><br>
+        Ulica: <input type="text" name="street" required>&nbsp
+        Numer domu: <input type="number" name="house_num" required>&nbsp
+        Numer mieszkania: <input type="number" name="apart_num" required><br><br>
+        Numer telefonu: <input type="number" name="phone_num" required><br><br>
+        <input type="checkbox" name="regular">Czy chcesz zapisać się do programu stałego klienta i otrzymywać specjalne zniżki?<br><br>
         <input type="submit" name="done" value="Zatwierdź">
     </form>
+    </div>
 
     <?php
     if(isset($_POST['name']) && isset($_POST['surname']) && isset($_POST['birthdate']) && isset($_POST['country']) && isset($_POST['city']) && isset($_POST['postal_code']) && isset($_POST['street']) && isset($_POST['house_num']) && isset($_POST['apart_num']) && isset($_POST['phone_num'])){
